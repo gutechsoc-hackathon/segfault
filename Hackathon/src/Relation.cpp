4399dc9fcba773f7694ef2b33808b0256a626301
michael-mckay <michael_mac_i@me.com>
2013-11-02 17:24:16 +0000

    Update Relation.cpp
    CHANGED 'RELATION' TO 'RELATIONSET'!!!

--- Hackathon/src/Relation.cpp
+++ Hackathon/src/Relation.cpp
@@ -1,6 +1,15 @@
 #include "Relation.h"
  /* The .cpp files are for source files and IMPLEMENTATION, i.e. not class defenitions! */
 
+std::list<unsigned long long>& get_in(int relation_type)
+{
+ return in[relation_type];
+}
+std::list<unsigned long long>& get_out(int relation_type)
+{
+ return out[relation_type];
+}
+
 void RelationSet :: add_in(int relation_type, unsigned long long id)
 {
  in[relation_type].push_back(id);
