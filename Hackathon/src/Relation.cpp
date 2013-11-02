2764f8dc5c7fa9f2d5bbd54c74d7c8a8294fad4f
michael-mckay <michael_mac_i@me.com>
2013-11-02 16:15:40 +0000

    Update Relation.cpp

--- Hackathon/src/Relation.cpp
+++ Hackathon/src/Relation.cpp
@@ -1,23 +1,2 @@
 #include "Relation.h"
-#include <list>
-
-class Relation {
-
-public:
-  
-  static const int friend_of = 0;
-  static const int married_to = 1;
-  static const int has_dated = 2;
-  static const int dislikes = 3;
-  static const int knows = 4;
-  
-  std::list<unsigned long long> in[5];
-  std::list<unsigned long long> out[5];
-  
-  std::list<unsigned long long> *get_in(int relation_type) { return &(in[relation_type]); }
-  std::list<unsigned long long> *get_out(int relation_type) { return &(out[relation_type]); }
-
-  void add_in(int relation_type, unsigned long long id) { in[relation_type].push_front(id); }
-  void add_out(int relation_type, unsigned long long id) { out[relation_type].push_front(id); }
-
-};
+ /* The .cpp files are for source files and IMPLEMENTATION, i.e. not class defenitions! */
