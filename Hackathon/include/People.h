1f5884dff5a4f09cf6057311fa17172a1c20a0fe
Joe Frew <JoeFrew95@gmail.com>
2013-11-02 14:46:45 +0000

    Committing changes to the People class
    Working on attempting to iterate over all people in the hashmap, and add
    all of their outgoing relationships to the corresponding person's
    incoming relationships.

--- Hackathon/include/People.h
+++ Hackathon/include/People.h
@@ -4,16 +4,20 @@
 
 class People
 {
+    typedef std::tr1::unordered_map< unsigned long long, Person > hashmap;
     public:
         People(fileloader* f);
         bool load();
-        Person get_person(unsigned long person_id);
+        Person get_person(unsigned long long person_id);
         int get_total_people();
         virtual ~People();
     protected:
     private:
+        hashmap peoplemap;
         unsigned int totalPeople;
+
         void add_people_to_map();
+        void get_person_at_index();
         void compute_incoming_relations();
 
 
--- Hackathon/src/People.cpp
+++ Hackathon/src/People.cpp
@@ -1,11 +1,32 @@
 #include "People.h"
 
-People::People()
-{
-    //ctor
+People::People(){
+    peoplemap =
 }
 
-People::~People()
-{
+People::~People(){
     //dtor
+}
+
+People::compute_incoming_relations(){
+    //we iterate over every person in the hashmap
+    //for each person, we get their outgoing relationships
+    //we then add this outgoing relationship to the incoming relationships of the corresponding person
+    for( hashmap::const_iterator i = numbers.begin(), e = numbers.end() ; i != e ; ++i ) {
+        //i->first
+        //i->second
+        Relation r = (i->second).get_relations();
+        //iterating over the relations this person has and adding them to the
+        for(int relationshiptype = 0; i<5; i++){
+            std::list<unsigned long long> relationships = *r.get_out(relationshiptype);
+            for(int k = 0; k<relationships.size(); k++){
+                //we are now iterating over the individual relationships. we are going to find the oerson in the peoplemap
+                //and add an incoming relationship for the person value currently at i->first when the relationship type is 'relationshiptype'
+                Person p = peoplemap[relationships.get(k)];
+                p.set_relations((p.get_relations()).add_in(relationshiptype, i->first));//adding the input value to the
+                peoplemap[]
+            }
+
+        }
+    }
 }
