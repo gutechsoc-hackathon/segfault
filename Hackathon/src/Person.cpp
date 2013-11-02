0c2f760088cde9d18324be870406609749b23782
Martynas <martynas.buivys@gmail.com>
2013-11-02 15:00:33 +0000

    Moved files to src folder

--- Hackathon/src/Person.cpp
+++ Hackathon/src/Person.cpp
@@ -1,11 +1,17 @@
-#include "Person.h"
 
-Person::Person()
-{
-    //ctor
-}
+class Person {
 
-Person::~Person()
-{
-    //dtor
+private:
+  
+  unsigned long long id;
+  Relation relation;
+
+public:
+  
+  unsigned long long get_id() { return id; }
+  Relation get_relation() { return relation; }
+  
+  void set_id(unsigned long long id) { this.id = id; }
+  void set_relation(Relation relation) { this.relation = relation; }
+
 }
--- Hackathon/src/Relation.cpp
+++ Hackathon/src/Relation.cpp
@@ -1,11 +1,22 @@
-#include "Relation.h"
+#include <list>
 
-Relation::Relation()
-{
-    //ctor
-}
+class Relation {
 
-Relation::~Relation()
-{
-    //dtor
-}
+public:
+  
+  static const int friend_of = 0;
+  static const int married_to = 1;
+  static const int has_dated = 2;
+  static const int dislikes = 3;
+  static const int knows = 4;
+  
+  std::list<unsigned long long> in[5];
+  std::list<unsigned long long> out[5];
+  
+  std::list<unsigned long long> *get_in(int relation_type) { return in[relation_type]; }
+  std::list<unsigned long long> *get_out(int relation_type) { return out[relation_type]; }
+
+  void add_in(int relation_type, unsigned long long id) { in[relation_type].push_front(id); }
+  void add_out(int relation_type, unsigned long long id) { out[relation_type].push_front(id); }
+
+};
