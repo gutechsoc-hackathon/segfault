bfcfc2f0c8f34146150eae8fa6a4ea040d008fa5
Martynas <martynas.buivys@gmail.com>
2013-11-02 15:10:45 +0000

    added an amperstand and got rid of compilation errors

--- Hackathon/src/Relation.cpp
+++ Hackathon/src/Relation.cpp
@@ -13,8 +13,8 @@ public:
   std::list<unsigned long long> in[5];
   std::list<unsigned long long> out[5];
   
-  std::list<unsigned long long> *get_in(int relation_type) { return in[relation_type]; }
-  std::list<unsigned long long> *get_out(int relation_type) { return out[relation_type]; }
+  std::list<unsigned long long> *get_in(int relation_type) { return &(in[relation_type]); }
+  std::list<unsigned long long> *get_out(int relation_type) { return &(out[relation_type]); }
 
   void add_in(int relation_type, unsigned long long id) { in[relation_type].push_front(id); }
   void add_out(int relation_type, unsigned long long id) { out[relation_type].push_front(id); }
