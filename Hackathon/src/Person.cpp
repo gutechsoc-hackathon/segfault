44f92b5053ba2c71c4ea5ad67b43348bae388864
Martynas <martynas.buivys@gmail.com>
2013-11-02 15:27:14 +0000

    fixed compilation errors

--- Hackathon/src/Person.cpp
+++ Hackathon/src/Person.cpp
@@ -1,17 +1,17 @@
 
 class Person {
-
-private:
   
+public:
+  
+
   unsigned long long id;
   Relation relation;
 
-public:
-  
+
   unsigned long long get_id() { return id; }
   Relation get_relation() { return relation; }
   
-  void set_id(unsigned long long id) { this.id = id; }
-  void set_relation(Relation relation) { this.relation = relation; }
+  void set_id(unsigned long long new_id) { id = new_id; }
+  void set_relation(Relation new_relation) { relation = new_relation; }
 
-}
+};
