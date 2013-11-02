84cb5a267427ffd9dc5c1f223b500cb62bdd88c0
michael-mckay <michael_mac_i@me.com>
2013-11-02 17:25:26 +0000

    Update Relation.h

--- Hackathon/include/Relation.h
+++ Hackathon/include/Relation.h
@@ -4,18 +4,18 @@
 #include <list>
 
 class RelationSet {
+private:
+   std::list<unsigned long long> in[5];
+   std::list<unsigned long long> out[5];
 public:
   static const int friend_of = 0,
     married_to = 1,
     has_dated = 2,
     dislikes = 3,
     knows = 4;
-  
-  std::list<unsigned long long> in[5];
-  std::list<unsigned long long> out[5];
-  
-  std::list<unsigned long long> *get_in(int relation_type);
-  std::list<unsigned long long> *get_out(int relation_type);
+    
+  std::list<unsigned long long>& get_in(int relation_type);
+  std::list<unsigned long long>& get_out(int relation_type);
 
   void add_in(int relation_type, unsigned long long id);
   void add_out(int relation_type, unsigned long long id);
