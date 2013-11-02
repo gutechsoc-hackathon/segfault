8baf46c90ce44baf7419fdba5ccb1a3cec73868a
rarewt <st.boca@gmail.com>
2013-11-02 14:59:04 +0000

    changed main to work with people/relation.cpp

--- main.cpp
+++ main.cpp
@@ -30,16 +30,22 @@ static PersonsLL *parse(FILE *fd) {
         //if not adding relations, then we are adding IDs
         if(!go) {
             long long unsigned idi = strtoull(bf, NULL, 10);
-            guy.setId(idi);
+            guy.set_id(idi);
         }
 
         //if we are adding relations, split by space, send (type, id) to person class
         else {
             char *part;
             part = strtok(bf, " ");
-            int type = atoi(part);
+            if(part == "FRIEND_OF") type = 0;
+            else if(part == "MARRIED_TO") type = 1;
+            else if(part == "HAS_DATED") type = 2;
+            else if(part == "DISLIKES") type = 3;
+            else if(part == "KNOWS") type = 4;
             long long unsigned idi = strtoull(strtok(NULL, "\n"), NULL, 10); //take next part of line, turn into llu and assign to idi
-            guy.setOutgoingRelation(type, idi);
+            Relation rel = (Relation)malloc(sizeof(Relation));
+            rel.add_out(type, idi);
+            guy.set_relation(rel);
         }
     }
 
