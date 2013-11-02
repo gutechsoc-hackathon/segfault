0929388883b43788d1b446587109d49651926783
michael-mckay <michael_mac_i@me.com>
2013-11-02 16:08:17 +0000

    Update Person.cpp

--- Hackathon/src/Person.cpp
+++ Hackathon/src/Person.cpp
@@ -1,18 +1,3 @@
 #include "Person.h"
 
-class Person {
-  
-public:
-  
-
-  unsigned long long id;
-  Relation relation;
-
-
-  unsigned long long get_id() { return id; }
-  Relation get_relation() { return relation; }
-  
-  void set_id(unsigned long long new_id) { id = new_id; }
-  void set_relation(Relation new_relation) { relation = new_relation; }
-
-};
+/* Methods are defined here. The class defenition goes in the HEADER FILE */
