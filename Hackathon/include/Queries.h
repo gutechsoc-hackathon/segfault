12d8fafc4ecd0c3e290b151c9caa5970f1ec8b34
michael-mckay <michael_mac_i@me.com>
2013-11-02 22:43:00 +0000

    Update Queries.h

--- Hackathon/include/Queries.h
+++ Hackathon/include/Queries.h
@@ -1,14 +1,30 @@
 #ifndef QUERIES_H
 #define QUERIES_H
 
+#include "people.h"
 
-class Queries
+class QueryCaller
 {
+	// This class is constructed with a People object, and will continue to use
+	// that people object for querying throughout it's existence.
+	private:
+		People& people;
     public:
-        Queries();
-        virtual ~Queries();
-    protected:
-    private:
+        QueryCaller(const People& people);
+        ~QueryCaller();
+
+		size_t howManyPeopleAreThere();
+		size_t averageNumberOfRelationshipsForOddEvenPeople();
+
+		size_t howManyNarcissists();
+		size_t betweenTwoPeopleHowManyFriendOfs();
+		Person& whoIsDislikedTheMost();
+
+		void drawVisualisation(); 
+		// I think it would be cool to print an ASCII art map to
+		// the terminal for this one.
+
+		Person& whichPersonHasTheLongestCycleOfFriendOfRelationships();
 };
 
 #endif // QUERIES_H
