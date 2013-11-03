5bd15abcd75f5d384e03066d9c0bc6a7abbcb3e1
martynasb <martynas.buivys@gmail.com>
2013-11-03 11:11:37 +0000

    Narcissist detector implementation

--- Hackathon/src/Queries.cpp
+++ Hackathon/src/Queries.cpp
@@ -18,6 +18,33 @@ size_t QueryCaller::howManyPeopleAreThere()
 
 size_t QueryCaller::howManyNarcissists()
 {
-	// auto peopleMap = people.peopleMap;
+	auto peopleMap = people.peopleMap;
+	unsigned long long narcissistsFound = 0;
+	bool relationshipFound;
+	unsigned int i;
+	
+	for (Hashmap::iterator person = peopleMap->begin(); person != peopleMap->end(); ++person)
+        {
+        	relationshipFound = false;
+        	i = 0;
+        	
+        	while (i < 5 && relationshipFound == false)
+        	{
+        		for (unsigned long long buddyPersonID : person->second.getRelationSet().out[i])
+        		{
+        			Hashmap::iterator buddyPersonIterator = peopleMap->find(buddyPersonID);
+        			
+        			if (buddyPersonID == person->first)
+        			{
+        				relationshipFound = true;
+        				narcissistsFound++;
+        				break;
+        			}
+        		}
+				
+        		i++;
+        	}
+        } 	
+	
 	return 0;
 }
