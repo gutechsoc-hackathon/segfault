3cf8ef19bd4a9e0b050c3ad57b0d27a428d4d840
michael-mckay <michael_mac_i@me.com>
2013-11-03 08:25:56 +0000

    Update People.h

--- Hackathon/include/People.h
+++ Hackathon/include/People.h
@@ -10,14 +10,14 @@ class People
     typedef std::unordered_map<unsigned long long, Person> Hashmap;
     private:
         Hashmap* peopleMap;
-        void compute_incoming_relations();
+        void computeIncomingRelations();
     public:
         People();
         ~People();
 
         bool load(std::list<Person>& l, bool analyse);
-        Person& get_person(unsigned long long person_id);
-        unsigned long long get_total_people();
+        Person& getPerson(unsigned long long person_id);
+        unsigned long long getTotalPeople();
 };
 
 #endif // PEOPLE_H
