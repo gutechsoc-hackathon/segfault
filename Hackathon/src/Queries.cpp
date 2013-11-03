32a7d19876aa2432659df86b2bb6e91b7415a4d2
michael-mckay <michael_mac_i@me.com>
2013-11-03 12:51:30 +0000

    Update Queries.cpp

--- Hackathon/src/Queries.cpp
+++ Hackathon/src/Queries.cpp
@@ -1,6 +1,9 @@
-#include "Queries.h"
-#include "Relation.h"
+#include "queries.h"
+#include "relation.h"
 #include <list>
+#include <iostream>
+#include <iomanip>
+#include <string>
 
 QueryCaller::QueryCaller(People& newPeople) :
 people(newPeople)
@@ -20,7 +23,7 @@ size_t QueryCaller::howManyPeopleAreThere()
 
 size_t QueryCaller::howManyNarcissists()
 {
-	auto peopleMap = people.peopleMap;
+	auto peopleMap = people.getPeopleMap();
 	unsigned long long narcissistsFound = 0;
 	bool relationshipFound;
 	unsigned int i;
@@ -51,7 +54,7 @@ size_t QueryCaller::howManyNarcissists()
 	return 0;
 }
 
-Person& QueryCaller::MostDisliked()
+Person& QueryCaller::whoIsDislikedTheMost()
 {
 	Person* personPointer = NULL;
 	int mostdislikes = 0;
@@ -132,4 +135,82 @@ std::pair<long, long> QueryCaller::averageNumberOfRelationshipsForOddEvenPeople(
 		result.second = oddRels / oddPeople;
 
 		return result;
+}
+
+void QueryCaller :: drawVisualisation()
+{
+	system("CLS");
+
+	size_t numberOfRelationships = 0;
+
+	auto peopleMap = people.getPeopleMap();
+	auto currentPerson = peopleMap->begin();
+
+	// Find noR.
+	for (size_t i = 0; i < 5; ++i)
+	{
+		numberOfRelationships += currentPerson->second.getRelationSet().out[i].size();
+	}
+
+
+	std::cout << "+ ";
+	std::cout << std::string(19,'-') << " +\n";
+
+	std::string relationshipTypenames[] = {
+		"is friends with-",
+		"is married to---",
+		"has dated-------",
+		"dislikes--------",
+		"knows-----------",
+
+		"is a friend of--",
+		"is married by---",
+		"was dated by----",
+		"is disliked by--",
+		"is known by-----"};
+
+	size_t currentRelationshipArrow = 0;
+
+	for (size_t currentRow = 0; currentRow < 13;
+		++currentRow)
+	{
+		std::cout << "| ";
+
+		if (currentRow == 5)
+		{
+			std::cout << std::setw(19) << std::setfill('0') 
+		<< currentPerson->first;
+			
+		}
+		else if (currentRow == 4)
+		{
+			std::cout <<
+				"Person:" << std::string(12,' ');
+		}
+		else
+		{
+			std::cout << std::string(19,' ');
+		}
+		std::cout << " |";
+
+		// Draw any relationships.
+
+		
+		if (currentRow % 3 == 0 && currentRelationshipArrow < 5)
+		{
+			// Draw a relationship.
+
+			std::cout << "----" << 
+				relationshipTypenames[currentRelationshipArrow]
+				<< "--->";
+
+			++currentRelationshipArrow;
+		}
+
+		std::cout << "\n";
+	}
+
+	std::cout << "+ ";
+	std::cout << std::string(19,'-') << " +\n";
+
 }
