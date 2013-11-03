1a70bcb9bb6cac778d86e887a490400c3037b448
michael-mckay <michael_mac_i@me.com>
2013-11-03 08:25:05 +0000

    Update People.cpp

--- Hackathon/src/People.cpp
+++ Hackathon/src/People.cpp
@@ -9,12 +9,12 @@ People::~People(){
 }
 
 //this function takes a pointer to a parser object and calls
-bool People::load(std::list<Person>& l, bool analyse){
+bool People::load(std::list<Person>& personList, bool analyse){
 	//l is a pointer to a list of ALL of the people loaded by the parser
 
 	//then we load each person in to the hashmap
-	for(Person& p : l){
-		peopleMap->emplace(p.get_id(),p);
+	for(Person& person : personList){
+		peopleMap->emplace(person.get_id(),person);
 	}
 
 	if(analyse){
@@ -23,11 +23,11 @@ bool People::load(std::list<Person>& l, bool analyse){
 	return true;
 }
 
-unsigned long long People :: get_total_people(){
+unsigned long long People :: getTotalPeople(){
 	return peopleMap ? peopleMap->size() : 0;
 }
 
-void People::compute_incoming_relations(){
+void People::computeIncomingRelations(){
 	//we iterate over every person in the hashmap
 	//for each person, we get their outgoing relationships
 	//we then add this outgoing relationship to the incoming relationships of the corresponding person
@@ -35,6 +35,7 @@ void People::compute_incoming_relations(){
 	for (Hashmap::iterator person = peopleMap->begin(); person != peopleMap->end(); ++person)
 	{
 		for (unsigned int i = 0; i < 5; ++i)
+		{
 			for (unsigned long long buddyPersonID : person->second.get_relationSet().out[i])
 			{
 				Person* buddyPerson = &(peopleMap->find(buddyPersonID)->second);
@@ -44,11 +45,11 @@ void People::compute_incoming_relations(){
 
 				// This code takes the old buddy's IN relations and appends the person's OUT relations.
 			}
+		}
 	}
 	
-
+}
 	/*
-
 	for( Hashmap::iterator i = peopleMap->begin(), e = peopleMap->end() ; i != e ; ++i ) {
 		//i->first
 		//i->second
@@ -71,4 +72,3 @@ void People::compute_incoming_relations(){
 		}
 	}
 	*/
-}
