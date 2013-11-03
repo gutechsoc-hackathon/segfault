4915fd9af9b9a87b1de56edfe62cc224ba828fb1
michael-mckay <michael_mac_i@me.com>
2013-11-03 08:21:57 +0000

    Update Person.cpp

--- Hackathon/src/Person.cpp
+++ Hackathon/src/Person.cpp
@@ -3,21 +3,21 @@
 
 /* Methods are defined here. The class defenition goes in the HEADER FILE */
 
-  unsigned long long Person :: getID() 
-  {
-    return id; 
-  }
-  
-  RelationSet& Person :: getRelationSet()
-  {
-    return relation;
-  }
-  
-  void Person :: setID(unsigned long long newID)
-  {
-    id = newID; 
-  }
-  void Person :: setRelationSet(RelationSet& newRelation)
-  {
-    relation = newRelation;
-  }
+unsigned long long Person :: getID() 
+{
+	return id; 
+}
+
+RelationSet& Person :: getRelationSet()
+{
+	return relation;
+}
+
+void Person :: setID(unsigned long long newID)
+{
+	id = newID; 
+}
+void Person :: setRelationSet(RelationSet& newRelation)
+{
+	relation = newRelation;
+}
