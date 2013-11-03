7906af75333f3f5a0403f41f0a0f90c32b18b806
michael-mckay <michael_mac_i@me.com>
2013-11-03 08:28:47 +0000

    Update Relation.cpp

--- Hackathon/src/Relation.cpp
+++ Hackathon/src/Relation.cpp
@@ -1,12 +1,12 @@
 #include "relation.h"
  /* The .cpp files are for source files and IMPLEMENTATION, i.e. not class defenitions! */
 
-void RelationSet :: add_in(int relation_type, unsigned long long id)
+void RelationSet :: addIn(int newRelationType, unsigned long long id)
 {
  in[relation_type].push_back(id);
 }
 
-void RelationSet :: add_out(int relation_type, unsigned long long id)
+void RelationSet :: addOut(int newRelationType, unsigned long long id)
 {
  out[relation_type].push_back(id);
 }
