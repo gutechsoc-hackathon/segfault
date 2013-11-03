0fb80e9f1fc9e5c239fdf4496e6fe509eb549983
michael-mckay <michael_mac_i@me.com>
2013-11-03 09:12:42 +0000

    Update Person.cpp

--- Hackathon/src/Person.cpp
+++ Hackathon/src/Person.cpp
@@ -21,3 +21,22 @@ void Person :: setRelationSet(RelationSet& newRelation)
 {
 	relation = newRelation;
 }
+
+std::string Person :: getStringRepresentation()
+{
+	std::stringstream  stringStream;
+	std::string relationshipTypenames[] = {
+		"is friends with ",
+		"is married to   ",
+		"has dated       ",
+		"dislikes        ",
+		"knows           "};
+
+	stringStream << "Person " << getID() << ":";
+	// Convert the long number into a cool name?
+	for (size_t i = 0; i < 5; ++i)
+		for (unsigned long long otherPerson : getRelationSet().out[i])
+			stringStream << "\n\t" << relationshipTypenames[i] << otherPerson;
+
+	return stringStream.str();
+}
